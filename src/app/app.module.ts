94445ea4627982713cbcaa0fa910202378c7fb6e
Oleg Shaliakin <oleg.shaliakin@NetCracker.com>
2021-07-08 15:04:42 +0400

    add admin module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {PostPageComponent} from './post-page/post-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { AdminLayoutComponent } from './admin/shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './admin/login-page/login-page.component';
+import {AdminModule} from "./admin/admin.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
