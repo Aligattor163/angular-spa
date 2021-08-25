9abdd466bebe5ce635a920195cd61abf57511203
Oleg Shaliakin <oleg.shaliakin@NetCracker.com>
2021-08-25 23:58:20 +0400

    renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularSPA'`, () => {
+  it(`should have as title 'angular-spa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularSPA');
+    expect(app.title).toEqual('angular-spa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularSPA app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-spa app is running!');
   });
 });
